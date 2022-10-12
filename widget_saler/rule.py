3790c0d0e1d7beeefbedd43814100c4ec352efc1
Wei Lee <weilee.rx@gmail.com>
2022-10-12 16:52:16 +0800

    refactor: merge delivery_cost and offer as rule

--- widget_saler/rule.py
+++ widget_saler/rule.py
@@ -1,6 +1,12 @@
 from typing import TypedDict
 
 
+class SepcialOfferRule(TypedDict):
+    product_code: str
+    amount: int
+    discount: float
+
+
 class DeliveryCostChargeRule(TypedDict):
     lower_than_threshold: float
     delivery_cost: float
