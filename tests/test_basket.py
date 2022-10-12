6ca4770bc5f21e16f2bb62aa9882e6eace09b0e1
Wei Lee <weilee.rx@gmail.com>
2022-10-12 16:43:25 +0800

    feat(widget_saler/basket): add Basket class skeleton and minimum test cases to it

---
+++ tests/test_basket.py
@@ -0,0 +1,20 @@
+import pytest
+
+from widget_saler.basket import Basket
+
+
+@pytest.mark.parametrize(
+    "product_codes, expected_total",
+    (
+        (("B01", "G01"), 37.85),
+        (("R01", "R01"), 54.37),
+        (("R01", "G01"), 60.85),
+        (("B01", "B01", "R01", "R01"), 98.25),
+    ),
+)
+def test_basket_total(product_codes: list[str], expected_total: float):
+    basket = Basket()
+    for product_code in product_codes:
+        basket.add(product_code)
+
+    assert basket.total == expected_total
