554947d7fe5ccf54eb487d564930f2c7b8079cab
Wei Lee <weilee.rx@gmail.com>
2022-10-13 22:26:28 +0800

    feat(basket_config): add load_json method for loading json configuration file

--- widget_saler/basket_config.py
+++ widget_saler/basket_config.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 
 from widget_saler.product import Product
@@ -13,3 +14,8 @@ class BasketConfig:
     def __post_init__(self) -> None:
         # ensure shipping_fee can check the threshold in correct order
         self.delivery_cost_rules.sort(key=lambda rule: rule["lower_than_threshold"])
+
+    @staticmethod
+    def load_json(filename: str) -> "BasketConfig":
+        with open(filename, "r", encoding="utf-8") as input_file:
+            return BasketConfig(**json.load(input_file))
